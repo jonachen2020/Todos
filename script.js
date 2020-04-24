3d57763ac92a8c42158afcf1d86da456e1c4b8ea
Jonathan Chen <jonachen2020@gmail.com>
2020-04-24 15:05:51 -0700

    Refactored button functions

--- script.js
+++ script.js
@@ -65,13 +65,15 @@ var todoList = {
     },
 }
 
-var displayTodosButton = document.getElementById("displayTodosButton")
-var toggleAllButton = document.getElementById("toggleAllButton")
+button.addEventListener('click', function(buttonId){
 
-displayTodosButton.addEventListener('click', function() { 
-    todoList.displayTodos()
 })
 
-toggleAllButton.addEventListener('click', function() {
-    todoList.toggleAll()
-})
+var handlers = {
+    displayTodos: function() {
+        todoList.displayTodos()
+    },
+    toggleAll: function() {
+        todoList.toggleAll()
+    },
+}
